e2dc94482502ce8bd2d34a44fb05bb836bfaaa89
Gabriel Hayes <gabriel.speed.bullock@gmail.com>
2022-03-22 10:24:58 -0500

    Fix connectors.

--- src/fire_duck/Connectors.hx
+++ src/fire_duck/Connectors.hx
@@ -10,8 +10,7 @@ package fire_duck;
 			client.augment({
 				before: [
 					req -> {
-						@:privateAccess req.header.fields.push(new HeaderField("x-access-token",
-							apiKey));
+						@:privateAccess req.header.fields.push(new HeaderField("x-access-token", apiKey));
 						req;
 					}
 				]
@@ -20,14 +19,21 @@ package fire_duck;
 					req -> {
 						res -> {
 							req.body.all().next(body -> {
-								log(req.header);
-								log(body);
-								res.body.all()
-									.next(body -> {
-										log(res.header);
-										log(body);
-										res;
-									});
+								fire_duck.Logger.log(req.header);
+								try {
+									fire_duck.Logger.log(body);
+								} catch (e) {
+									fire_duck.Logger.log('Couldnt print body: ${e.details()}');
+								}
+								res.body.all().next(body -> {
+									fire_duck.Logger.log(res.header);
+									try {
+										fire_duck.Logger.log(body);
+									} catch (e) {
+										fire_duck.Logger.log('Couldnt print body: ${e.details()}');
+									}
+									res;
+								});
 							});
 						}
 					}
@@ -38,29 +44,28 @@ package fire_duck;
 	};
 
 	@:await static function firebaseInit() {
-		final cfg = haxe.Json.parse(@:await
-			boisly.AppSettings.config.firebase.svcCfg);
-		if (AppSettings.config.firebase.standalone)
+		final cfg = haxe.Json.parse(@:await boisly.AppSettings.config.firebase.svcCfg);
+		trace('w0t');
+		trace(FirebaseAdmin.apps);
+		if (AppSettings.config.firebase.standalone && FirebaseAdmin.apps.length <= 1) {
+			trace('Initializing app...');
 			FirebaseAdmin.initializeApp({
 				credential: firebase_admin.Credential.cert(cfg) // databaseURL: boisly.AppSettings.config.firebase.databaseURL
 			});
-		FirebaseAdmin.initializeApp({
-			credential: firebase_admin.Credential.cert(cfg) // databaseURL: boisly.AppSettings.config.firebase.databaseURL
-		}, "fireduck");
+
+			FirebaseAdmin.initializeApp({
+				credential: firebase_admin.Credential.cert(cfg) // databaseURL: boisly.AppSettings.config.firebase.databaseURL
+			}, "fireduck");
+		}
 	}
 
-	public static var duck:Promise<bp.duck.Proxy> = getClient.next(client ->
-		tink.Web.connect((AppSettings.config.duckApiUrl : bp.duck.proxy.WildDuckProxy),
-		{
-			client: client
-		}));
-	public static var pkapi:Promise<tink.web.proxy.Remote<PKAPI>> = getClient.next(client ->
-		tink.Web.connect(('https://www.googleapis.com' : PKAPI),
-			{
+	public static var duck:Promise<bp.duck.Proxy> = getClient.next(client -> tink.Web.connect((AppSettings.config.duckApiUrl : bp.duck.proxy.WildDuckProxy), {
 		client: client
 	}));
-	public static var publicKeys:Promise<haxe.DynamicAccess<String>> = pkapi.next(api ->
-		api.getPublicKeys());
+	public static var pkapi:Promise<tink.web.proxy.Remote<PKAPI>> = getClient.next(client -> tink.Web.connect(('https://www.googleapis.com' : PKAPI), {
+		client: client
+	}));
+	public static var publicKeys:Promise<haxe.DynamicAccess<String>> = pkapi.next(api -> api.getPublicKeys());
 }
 
 interface PKAPI {
